4a9f8a0a96967d193527cdc9d9246d773c94d0de
jorge-sa <lucassairmaos@gmail.com>
2023-09-27 20:50:34 -0300

    medium problem structure update

--- jorge_human/greatest_common_divisors_linked_list(medium).py
+++ jorge_human/greatest_common_divisors_linked_list(medium).py
@@ -1,17 +1,16 @@
-class Solution:    
-    def gcd(self, val1, val2):
-        gcd = 1
-        for n in range(2, min(val1, val2)+1):
-            if val1%n == 0 and val2%n == 0:
-                gcd = n
+class Solution:        
+    def insertGreatestCommonDivisors(self, list:listNode):
+        def gcd(val1, val2):
+            gcd = 1
+            for n in range(2, min(val1, val2)+1):
+                if val1%n == 0 and val2%n == 0:
+                    gcd = n
+            return gcd
         
-        return gcd
-    
-    def insert_gcd_nodes(self, list:listNode):
         current = list
         while current.next:
             val1, val2 = current.val, current.next.val
             dummy = current.next
-            current.next = listNode(self.gcd(val1, val2))
+            current.next = listNode(gcd(val1, val2))
             current.next.next = dummy
             current = current.next.next
